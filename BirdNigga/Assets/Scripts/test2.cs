c0ff92719d4bd01260c3b91c53f6a14edcf1e634
M-Hamid-Saeed <hamid.saeed.uni@gmail.com>
2024-01-24 21:16:13 +0500

    ......

--- BirdNigga/Assets/Scripts/test2.cs
+++ BirdNigga/Assets/Scripts/test2.cs
@@ -9,17 +9,40 @@ public class test2 : MonoBehaviour
 float yDegrees=0;
  
 public float speed=4; // allow us to adjust as we run
- 
-void Start() {
+
+    private void Awake()
+    {
+        Debug.Log("IN awkae");
+    }
+    void Start() {
   baseSpin=transform.rotation;
+        Debug.Log("IN START");
 }
  
 void Update() {
-  Quaternion ySpin=Quaternion.Euler(0, yDegrees,0);
-  yDegrees+=speed;
- 
-  transform.rotation=baseSpin * ySpin;
-  //transform.rotation =ySpin*baseSpin;
-}
+        /*Quaternion ySpin=Quaternion.Euler(0, yDegrees,0);
+        yDegrees+=speed;*/
+        Debug.Log("In Update");
+
+        //transform.rotation=baseSpin * ySpin;
+        //transform.rotation =ySpin*baseSpin;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log(transform.forward);
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log(transform.right);
+        }
+        
+    }
+    
+    private void OnEnable()
+    {
+
+    
+        Debug.Log("In Enable");
+    }
 
 }
